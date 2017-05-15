b634b30092a000cc84bf48d70f7d74e9290f12d1
Tri Pham <pvtri96@gmail.com>
2017-05-15 14:45:48 +0700

    change css to scss

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import Header from './Header';
 import Head from 'next/head';
+import stylesheet from 'style/styles.scss'
 
 const AppLayout = (props) => (
   <div>
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
     <Header></Header>
     <div className="container">
