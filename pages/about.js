3a17313c9399a368381c0911378a975f5dfce30e
Thị Thùy Dung Lê <Th Thy Dung L>
2017-05-15 20:47:40 +0700

    Overview netjs

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import AppLayout from '../components/AppLayout';
+
+const About = () => (
+  <AppLayout>
+    <div>About page</div>
+  </AppLayout>
+)
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import AppLayout from '../components/AppLayout';
+import Link from 'next/link';
 
 const Index = () => (
   <AppLayout>
     <div>Hello World</div>
+    <Link href="/about">
+      <a>Click to About</a>
+    </Link>
   </AppLayout>
 )
 
