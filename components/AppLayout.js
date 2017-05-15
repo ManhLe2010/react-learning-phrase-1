9a5be0188e4e67a46f799756a88079be9b804f23
lednhatkhanh <lednhatkhanh@gmail.com>
2017-05-15 13:43:23 +0700

    Done adding example

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,5 +1,11 @@
+import Head from 'next/head';
+
 const AppLayout = (props) => (
   <div>
+    <Head>
+      {/* Lam cach nay, tat ca cac children cua file AppLayout deu se co css cua Bootstap */}
+      <link rel='stylesheet' href='/static/css/bootstrap.min.css' />
+    </Head>
     {props.children}
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 import AppLayout from '../components/AppLayout';
 
 const Index = () => (
   <AppLayout>
+    {/* Neu Head duoc su dung o day, se chi minh index la co css cua bootstrap */}
     <div>Hello World</div>
+    <Link href='/about' prefetch><a>About</a></Link>
   </AppLayout>
 )
 
